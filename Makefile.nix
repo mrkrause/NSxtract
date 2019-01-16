6b8f88394e8838265d64d7d6c3125d6f3ae65ba0
Matt Krause <mrkrause@gmail.com>
2019-01-16 00:44:35 -0500

    Updated Makefile for linux

--- Makefile.nix
+++ Makefile.nix
@@ -1,18 +1,22 @@
-MATLAB_ROOT=/usr/local/MATLAB/R2015a/
+MATLAB_ROOT=/usr/local/MATLAB/R2018a
 CC=$(MATLAB_ROOT)/bin/mex
-CFLAGS=-client engine -g -DMAT_FILE_SUPPORT
+CFLAGS=-v GCC=/usr/bin/gcc-5 -client engine -g -DMAT_FILE_SUPPORT
 LIBS=-lFLAC -lFLAC++ -lboost_program_options -lboost_filesystem -lboost_system
 TARGET = rippleToFlac
 DEPS = NSxFile.h NSxConfig.h MatFile.h
-OBJ = typeHelper.o MatFile.o NSxConfig.o NSxFile.o NSxChannel.o NSxHeader.o nsx2mat.o nsx2txt.o rippleToFlac.o
+OBJ = NSxConfig.o NSxFile.o NSxChannel.o NSxHeader.o nsx2mat.o nsx2txt.o rippleToFlac.o
+COMMON_OBJ = typeHelper.o MatFile.o
 
 
 %.o: %.cpp $(DEPS)
 	$(CC) -c $@ $< $(CFLAGS)
 
-rippleToFlac: $(OBJ)
+rippleToFlac: $(COMMON_OBJ) NSxConfig.o NSxFile.o NSxChannel.o NSxHeader.o nsx2mat.o nsx2txt.o rippleToFlac.o
 	$(CC) -output $@ $^ $(CFLAGS) $(LIBS)
 
+NEVExtract: $(COMMON_OBJ) datapacket.o NEVConfig.o NEVFile.o extheader.o NEVExtract.o saveNEVEvents.o
+	$(CC) -output $@ $^ $(CFLAGS) $(LIBS)
+	
 .PHONY: clean
 clean:
 	rm -f *.o *~ core
